5c10a756c6065d7f1bd7b50d9fa054c4a8429b77
gapiyka <50524296+gapiyka@users.noreply.github.com>
2022-02-10 20:50:32 +0200

    solving equaction + add interective mode
    Realize function to solve quadratic equation by discriminant
    Interactive (Console) mode to input equation values.

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -6,21 +6,62 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            int[] equationArgs = ReadConsoleArguments();
-
+            double[] equationArgs = ReadConsoleArguments();
+            Console.WriteLine("Quadratic equation is: " + equationArgs[0] + "*" +
+                "x^2 + (" + equationArgs[1] + ")*x + (" + equationArgs[2] + ")");
+            SolveEquation(equationArgs[0], equationArgs[1], equationArgs[2]);
             PauseConsole();
         }
 
-        private static int[] ReadConsoleArguments()
+        private static double[] ReadConsoleArguments()
         {
             Console.WriteLine("Input numbers of Quadratic Equation!");
-            Console.Write("a: ");
-            var aInput = Console.ReadLine();
-            Console.Write("b: ");
-            var bInput = Console.ReadLine();
-            Console.Write("c: ");
-            var cInput = Console.ReadLine();
-            return new int[3] { Int32.Parse(aInput), Int32.Parse(bInput), Int32.Parse(cInput) };
+            double aInput = TryReadNum("a: ");
+            double bInput = TryReadNum("b: ");
+            double cInput = TryReadNum("c: ");
+            return new double[3] { aInput, bInput, cInput };
+        }
+
+        private static double TryReadNum(string val)
+        {
+            Console.Write(val);
+            var input = Console.ReadLine();
+            if (!double.TryParse(input, out double parsedInput))
+            {
+                Console.WriteLine("Invalid number value, entered '" + input + "' instead");
+                return TryReadNum(val);
+            }
+            else
+            {
+                return parsedInput;
+            }
+        }
+
+            private static void SolveEquation(double a, double b, double c)
+        {
+            double x1, x2;
+            double discr = b * b - 4 * a * c;
+            double sqrt = Math.Sqrt(discr);
+            string resultString;
+
+            if (discr > 0)
+            {
+                x1 = (-b + sqrt) / (2 * a);
+                x2 = (-b - sqrt) / (2 * a);
+
+                resultString = "Result: x1=" + x1 + "; x2=" + x2;
+            }
+            else if (discr < 0)
+            {
+                resultString = "Result: No roots";
+            }
+            else
+            {
+                x1 = (-b) / (2 * a);
+                resultString = "Result: x=" + x1;
+            }
+
+            Console.WriteLine(resultString);
         }
 
         private static void PauseConsole()

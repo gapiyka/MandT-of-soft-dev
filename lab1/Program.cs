62356c9e4b32f02806981c25aa4b8968422a71d5
gapiyka <50524296+gapiyka@users.noreply.github.com>
2022-02-10 18:29:27 +0200

    add scan of arguments from console
    Common console reading for getting arguments

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -6,7 +6,26 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] equationArgs = ReadConsoleArguments();
+
+            PauseConsole();
+        }
+
+        private static int[] ReadConsoleArguments()
+        {
+            Console.WriteLine("Input numbers of Quadratic Equation!");
+            Console.Write("a: ");
+            var aInput = Console.ReadLine();
+            Console.Write("b: ");
+            var bInput = Console.ReadLine();
+            Console.Write("c: ");
+            var cInput = Console.ReadLine();
+            return new int[3] { Int32.Parse(aInput), Int32.Parse(bInput), Int32.Parse(cInput) };
+        }
+
+        private static void PauseConsole()
+        {
+            Console.ReadKey();
         }
     }
 }
